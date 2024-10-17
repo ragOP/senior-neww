39b6e7fe324fff4e0660011908b3186a9461ab9b
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2024-10-18 03:12:50 +0530

    Add path ,ringba and tel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import First from "./pages/First";
+import Unlo from "./pages/unlockk";
+import Terms from "./pages/terms";
+import Privacy from "./pages/Privacy";
+
+
 
 function App() {
   return (
@@ -7,6 +12,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<First />} />
+          <Route path="/sfmedi-unlock" element={< Unlo/>} />
+          <Route path="/terms-and-conditions" element={< Terms/>} />
+          <Route path="/privacy-policy" element={< Privacy/>} />
         </Routes>
       </div>
     </Router>
